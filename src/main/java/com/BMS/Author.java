b21f79935a67759decd539d79211d2dc9771d545
Lungelo Hlatshwayo <ianhlatshwayo@gmail.com>
2024-04-22 19:26:50 +0200

    first commit

--- src/main/java/com/BMS/Author.java
+++ src/main/java/com/BMS/Author.java
@@ -1,15 +1,22 @@
 package com.BMS;
 
+import java.util.Optional;
+
 public class Author {
     private String name;
+    private Optional<String> wikipediaUrl;
 
 
-    public Author(String name) {
+    public Author(String name , Optional<String> wikipediaUrl) {
         this.name = name;
+        this.wikipediaUrl = wikipediaUrl;
     }
 
     public String getName() {
         return name;
     }
 
+    public Optional<String> getWikipediaUrl() {
+        return wikipediaUrl;
+    }
 }
--- src/main/java/com/BMS/Book.java
+++ src/main/java/com/BMS/Book.java
@@ -1,16 +1,20 @@
 package com.BMS;
 
+import java.util.Optional;
+
 public class Book {
     private String isbn;
     private Author author;
     private String title;
     private String status;
+    private Optional<String> imageUrl;
 
-    public Book(String isbn, Author author, String title) {
+    public Book(String isbn, Author author, String title ,Optional<String> imageUrl ) {
         this.isbn = isbn;
         this.author = author;
         this.title = title;
         this.status = "Available";
+        this.imageUrl = imageUrl;
     }
 
     public Author getAuthor() {
@@ -25,12 +29,13 @@ public class Book {
         this.status = status;
     }
 
-    public String stringValue(){
+    public String stringValue() {
         return "Book{" +
                 "isbn='" + isbn + '\'' +
                 ", author=" + author +
                 ", title='" + title + '\'' +
                 ", status='" + status + '\'' +
+                ", imageUrl='" + imageUrl.orElse("N/A") + '\'' + // Default value if not present
                 '}';
     }
 }
